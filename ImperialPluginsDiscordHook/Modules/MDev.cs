d0adcf8b9b9d584fceb4ad98c5293958948ad0ad
papershredder432 <34615274+papershredder432@users.noreply.github.com>
2025-01-11 17:35:51 -0500

    minor cleanup

--- ImperialPluginsDiscordHook/Modules/MDev.cs
+++ ImperialPluginsDiscordHook/Modules/MDev.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
@@ -75,7 +74,7 @@ public class MDev : InteractionModuleBase<SocketInteractionContext>
     }
 
     [SlashCommand("getcustomerservers", "Gets all servers of a customer.")]
-    public async Task FakeTicket(string user)
+    public async Task GetCustomerServers(string user)
     {
         var ipUser = _ipManagerService.GetUserAsync(user);
         if (ipUser == null)
@@ -102,22 +101,6 @@ public class MDev : InteractionModuleBase<SocketInteractionContext>
         
         try
         {
-
-            /*
-            var serverNum = 0;
-            var embedToSend = new EmbedBuilder()
-                .WithTitle($"{ipUser.UserName}'s Servers")
-                .WithDescription($"Servers {serverNum+1}/{servers.Count}")
-                .WithColor(Color.Blue)
-                .AddField("Server ID", servers[0].id, true)
-                .AddField("Server Address:Port", $"{servers[0].Host}:{servers[0].Port}", true)
-                .AddField("Whitelist Status", servers[0].isWhitelisted, true)
-                .AddField("First Seen", servers[0].registrationTime, true)
-                .AddField("Last Seen", servers[0].lastActivityTime, true)
-                .AddField("Whitelist Time", servers[0].whitelistTime != null ? servers[0].whitelistTime : "N/A", true)
-                .AddField("Products", string.Join(", ", serverProductNames), true);
-            */
-            
             var menuBuilder = new SelectMenuBuilder()
                 .WithPlaceholder($"Select a server.")
                 .WithCustomId($"server_menu_select_{user}");
@@ -129,44 +112,6 @@ public class MDev : InteractionModuleBase<SocketInteractionContext>
             }
             
             await RespondAsync(components: new ComponentBuilder().WithSelectMenu(menuBuilder).Build(), ephemeral: true);
-
-
-            /*
-            var embedsNeeded = Math.DivRem(servers.Count*7, 21, out var remainder);
-            if (remainder > 0) embedsNeeded++;
-            var fieldsCount = 0;
-            await _loggingService.LogVerbose(ELogType.Debug, $"Expecting {embedsNeeded} embeds. {embedsNeeded-1} \"full\" embeds and {remainder/7} servers in the last embed.");
-
-            var embedsToSend = new List<Embed>();
-            for (int i = 0; i < embedsNeeded; i++)
-            {
-                await _loggingService.LogVerbose(ELogType.Debug, $"Creating embed {i+1}/{embedsNeeded}.");
-
-                var embedToSend = new EmbedBuilder()
-                    .WithTitle($"{ipUser.UserName}'s Servers")
-                    .WithDescription($"Servers /{servers.Count}")
-                    .WithColor(Color.Blue);
-
-                for (int j = 0; j < 3; j++)
-                {
-                    await _loggingService.LogVerbose(ELogType.Debug, $"Creating fields {(j+1)*7}/21");
-                    fieldsCount += 7;
-
-                    embedToSend
-                        .AddField("Server ID", servers[fieldsCount%7].id, true)
-                        .AddField("Server Address:Port", $"{servers[fieldsCount%7].Host}:{servers[fieldsCount%7].Port}", true)
-                        .AddField("Whitelist Status", servers[fieldsCount%7].isWhitelisted, true)
-                        .AddField("First Seen", servers[fieldsCount%7].registrationTime, true)
-                        .AddField("Last Seen", servers[fieldsCount%7].lastActivityTime, true)
-                        .AddField("Whitelist Time", servers[fieldsCount%7].whitelistTime != null ? servers[fieldsCount%7].whitelistTime : "N/A", true)
-                        .AddField("Products", string.Join(", ", serverProductNames), true);
-                }
-
-                embedsToSend.Add(embedToSend.Build());
-            }
-            await _loggingService.LogVerbose(ELogType.Debug, $"Attempting to send {embedsToSend.Count} embeds.");
-            await RespondAsync(embeds: embedsToSend.ToArray(), ephemeral: true);
-            */
         } catch (Exception e)
         {
             await RespondAsync(e.Message, ephemeral: true);
--- Readme.md
+++ Readme.md
@@ -3,15 +3,14 @@
 ## Yes it's bad. I haven't made a Discord.Net bot in a while.
 
 ## Features
- ***Basically all of this is untested as I do not have an API key yet***
 - [x] Login to ImperialPlugins with an API key
 - [x] Get a list of all your products, customers, and their servers
 - [x] Show all active promotions (coupons)
     - [x] A blacklist of promotions to not show (coupons for individual customers)
 - [ ] Whitelist and blacklist servers
-- [ ] Sends you a message when a customer requests a whitelist
+- [X] Sends you a message when a customer opens a ticket (checks every 5 minutes, returns all unread tickets)
   - [ ] Shows a modal with the server information in question
-  - [ ] Allows you to respond via the modal (accept, deny, reply, etc.)
+  - [ ] Allows you to respond via the modal (accept/deny whitelists, reply, etc.)
 - [ ] Lets customers register in your Discord server for special roles (Customer role and per product roles)
 
 <br>
